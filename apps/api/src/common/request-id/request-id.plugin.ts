751463fc92f7f6a2a6ad16cf148223a0e06870de
rickonetik <rickonetik@gmail.com>
2026-01-23 04:55:23 -0800

    fix: set x-request-id header in onRequest hook (Story 0.6)
    - Move header setting to onRequest hook for proper timing
    - Header is now set before response is sent

--- apps/api/src/common/request-id/request-id.plugin.ts
+++ apps/api/src/common/request-id/request-id.plugin.ts
@@ -11,13 +11,7 @@ declare module 'fastify' {
 async function requestIdPlugin(fastify: any) {
   fastify.decorateRequest('traceId', '');
 
-  fastify.addHook('onRequest', async function (request: FastifyRequest) {
-    const incomingId = request.headers['x-request-id'];
-    request.traceId = typeof incomingId === 'string' ? incomingId : randomUUID();
-    request._startAt = Date.now();
-  });
-
-  fastify.addHook('onSend', async function (request: FastifyRequest, reply: FastifyReply) {
+  fastify.addHook('preHandler', async function (request: FastifyRequest, reply: FastifyReply) {
     reply.header('x-request-id', request.traceId);
   });
 }
